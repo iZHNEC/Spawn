85e2a11ca76b26e09eda40f640e1d55a732dfb73
Stanislav Shibilin <119742408+iZHNEC@users.noreply.github.com>
2023-04-23 17:45:28 +0300

    Spawn
    0.4

--- Assets/Scripts/SpawnHuman.cs
+++ Assets/Scripts/SpawnHuman.cs
@@ -6,25 +6,25 @@ public class SpawnHuman : MonoBehaviour
 {
     [SerializeField] private Enemy _enemyHuman;
 
-    private int maxEnemy = 5;
-    private float timeSpawn = 2f;
-    private float distance = 3;
-    private float timer;
+    private int _maxEnemy = 5;
+    private float _timeSpawn = 2f;
+    private float _distance = 3;
+    private float _timer;
 
     private void Start()
     {
-        timer = timeSpawn;
+        _timer = _timeSpawn;
     }
 
     private void Update()
     {
-        timer -= Time.deltaTime;
-        if (timer <= 0)
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
         {
-            timer = timeSpawn;
-            if (transform.childCount < maxEnemy)
+            _timer = _timeSpawn;
+            if (transform.childCount < _maxEnemy)
             {
-                Instantiate(_enemyHuman, Random.insideUnitCircle * distance, Quaternion.identity, transform);
+                Instantiate(_enemyHuman, Random.insideUnitCircle * _distance, Quaternion.identity, transform);
             }
         }
     }
